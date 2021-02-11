62fada23f06b88196607607da4925245461ed7be
digiter <6259181+digiter@users.noreply.github.com>
2021-02-11 13:39:43 -0800

    Create 1458-max-dot-product-of-two-subsequences.py

---
+++ 1458-max-dot-product-of-two-subsequences.py
@@ -0,0 +1,20 @@
+# 1458. Max Dot Product of Two Subsequences
+# O(500 ** 2)
+class Solution:
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        # Subsequences of nums1 and nums2 can be empty.
+        @cache
+        def solve(x, y):
+            if x == -1 or y == -1:
+                return 0
+            ans = solve(x - 1, y - 1) + max(0, nums1[x] * nums2[y])
+            ans = max(ans, solve(x, y - 1))
+            ans = max(ans, solve(x - 1, y))
+            return ans
+
+        # Guarantees non-empty by enumerating a pair of nums1[x] and nums2[y].
+        return max(
+            nums1[x] * nums2[y] + solve(x - 1, y - 1)
+            for x in range(len(nums1))
+            for y in range(len(nums2))
+        )
